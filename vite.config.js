42a0729e615ef8380b08051ab08f2b104f9bd051
allen2025dg <19198101@qq.com>
2026-03-02 19:43:16 +0800

    online ali ver

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ export default defineConfig({
   // ⚠️ 如果仓库名不是用户名.github.io，必须设置 base
   // 例如：仓库名是 threejs-demo，就填 '/threejs-demo/'
 
-   base: '/BN001-Corner-storage-rack/', // 注意开头和结尾的斜杠
-  //  base: '/',   // 临时改成根路径，方便本地测试
+  //  base: '/BN001-Corner-storage-rack/', // 注意开头和结尾的斜杠
+   base: '/',   // 临时改成根路径，方便本地测试
    build: {
     outDir: 'dist',
   }
